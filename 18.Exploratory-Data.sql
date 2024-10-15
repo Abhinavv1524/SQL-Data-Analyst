2f0684f37a6c8f687ecdbfeeba43669e911b562c
Abhinavv1524 <choudharyabhinav818@gmail.com>
2024-10-15 18:56:15 +0530

    Exploratory Data in SQL

--- 18.Exploratory-Data.sql
+++ 18.Exploratory-Data.sql
@@ -56,3 +56,40 @@ WHERE SUBSTRING(`date`,1,7) IS NOT NULL
 GROUP BY months
 ORDER BY 1 ASC;
 
+WITH Rolling_total AS
+(
+SELECT SUBSTRING(`date`,1,7) AS months, SUM(total_laid_off) AS total_off
+FROM layoff_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY months
+ORDER BY 1 ASC
+)
+SELECT months, total_off,
+SUM(total_off) OVER(ORDER BY months) AS rolling_total
+FROM Rolling_total;
+
+SELECT company, SUM(total_laid_off)
+FROM layoff_staging2
+GROUP BY company
+ORDER BY 2 DESC;
+
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoff_staging2
+GROUP BY company, YEAR(`date`)
+ORDER BY 3 DESC;
+
+WITH Company_year(company, years, total_laid_off) AS
+(
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoff_staging2
+GROUP BY company, YEAR(`date`)
+), Company_year_rank AS
+(
+SELECT * , DENSE_RANK () OVER(PARTITION BY years ORDER BY total_laid_off DESC) AS rankings
+FROM Company_year
+WHERE years IS NOT NULL
+)
+SELECT *
+FROM  Company_year_rank
+WHERE rankings <= 5;
+;
